d8923f0fb0e39b3e4f3d2dcddf39f37b0f0a1538
r!wehh <118078284+r1weh@users.noreply.github.com>
2023-11-29 02:00:40 +0700

    Create cl_npcinteract.lua
    Fixed For Blips in NPC Interact

---
+++ cl/cl_npcinteract.lua
@@ -0,0 +1,72 @@
+--[Example Coordonate]--
+-- {coords, Text, Heading, EntityNpc, ModelNpc} --
+local coordonate = {
+    {-427.77, 1115.03, 325.77, "#Rnr-SHOP!", 344.94, 0x62CC28E2, "s_m_y_armymech_01"}, --Disnaker
+}
+
+Citizen.CreateThread(function()
+Wait(1)
+    for _,v in pairs(coordonate) do
+      print(v[7])
+      RequestModel(GetHashKey(v[7]))
+      while not HasModelLoaded(GetHashKey(v[7])) do
+        Wait(1)
+      end
+  
+      RequestAnimDict("mini@strip_club@idles@bouncer@base")
+      while not HasAnimDictLoaded("mini@strip_club@idles@bouncer@base") do
+        Wait(1)
+      end
+      ped =  CreatePed(4, v[6],v[1],v[2],v[3], 3374176, false, true)
+      SetEntityHeading(ped, v[5])
+      FreezeEntityPosition(ped, true)
+      SetEntityInvincible(ped, true)
+      SetBlockingOfNonTemporaryEvents(ped, true)
+      TaskPlayAnim(ped,"mini@strip_club@idles@bouncer@base","base", 8.0, 0.0, -1, 1, 0, 0, 0, 0)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        local pos = GetEntityCoords(PlayerPedId(), true)
+        Citizen.Wait(0)
+        for _,v in pairs(coordonate) do
+            x = v[1]
+            y = v[2]
+            z = v[3]
+            if(Vdist(pos.x, pos.y, pos.z, x, y, z) < 20.0)then
+                DrawText3D(x,y,z+2.10, "~g~"..v[4], 1.2, 1)
+                DrawText3D(x,y,z+1.95, "", 1.0, 1)
+            end
+        end
+    end
+end)
+
+
+function DrawText3D(x,y,z, text, scl, font) 
+
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
+ 
+    local scale = (1/dist)*scl
+    local fov = (1/GetGameplayCamFov())*100
+    local scale = scale*fov
+   
+    if onScreen then
+        SetTextScale(0.0*scale, 1.1*scale)
+        SetTextFont(font)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 255)
+        SetTextDropshadow(0, 0, 0, 0, 255)
+        SetTextEdge(2, 0, 0, 0, 150)
+        SetTextDropShadow()
+        SetTextOutline()
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        AddTextComponentString(text)
+        DrawText(_x,_y)
+    end
+end
+
+--[[ENJOY]]--
