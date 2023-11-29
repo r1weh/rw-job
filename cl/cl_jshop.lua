ac9ae326dd5c85f4afef01c7b6a77187a0a8a152
r!wehh <118078284+r1weh@users.noreply.github.com>
2023-11-29 11:14:04 +0700

    Update Menu Context Shop!

--- cl/cl_jshop.lua
+++ cl/cl_jshop.lua
@@ -1,43 +1,87 @@
 RegisterNetEvent('rw:jualbahands')
 AddEventHandler('rw:jualbahands', function()
     lib.registerContext({
-        id = 'rw:jsbahan',
-        title = 'Menu Shop #KOTALAMA',
+        id = 'rw:jsbahan11',
+        title = 'Menu #KOTALAMA',
 		menu = 'rw:jsbahan',
         options = {
             {
+				title = 'Menu Kerja',
+				description = 'Menu Pekerjaan',
+                		icon = 'hand',
+				menu = 'joblist',
+			},
+            {
+				title = 'Menu Shop #KOTALAMA',
+				description = 'Jual Bahan',
+				menu = 'jualbahan',
+                		icon = 'hand',
+			},
+        }
+    })
+	lib.registerContext({
+		id = 'jualbahan',
+		title = 'Menu Shop #KOTALAMA',
+		menu = 'rw:jsbahan1',
+		onBack = function()
+			lib.showContext('rw:jsbahan11')
+		end,
+		options = {
+            {
 				title = 'Jual Bahan [Tambang]',
 				description = 'Kamu Bisa Jual Bahan Tambang Disini lo',
-                icon = 'hand',
+                		icon = 'hand',
 				event = 'rw:jsbahantambgn',
 			},
             {
 				title = 'Jual Bahan [Ayam]',
-				description = 'Kamu Bisa Jual Bahan Ayam Disini lo',
-				event = 'rw:ironc',
-                icon = 'hand',
+				description = 'Kamu Harus memiliki 10 Bahan Ayam',
+				event = 'rw:jualayam',
+                		icon = 'hand',
 			},
 			{
 				title = 'Jual Bahan [Kayu]',
 				description = 'Kamu Bisa Jual Bahan Kayu Disini lo',
 				event = 'rw:goldc',
-                icon = 'hand',
+                		icon = 'hand',
             },
 			{
 				title = 'Jual Bahan [Pakain]',
 				description = 'Kamu Bisa Jual Bahan Pakain Disini lo',
-				event = 'rw:copperc',
-                icon = 'hand',
+				event = 'rw:jualkain1',
+                		icon = 'hand',
+			},
+			{
+				title = 'Jual Bahan [Minyak]',
+				description = 'Kamu Bisa Jual Bahan Minyak Disini lo',
+				event = 'rw:jualminyak1',
+                		icon = 'hand',
 			},
 			{
 				title = 'Jual Bahan [Tani]',
 				description = 'COMMING SOON',
 				event = 'rw:diamondc',
-                icon = 'hand',
+                		icon = 'hand',
 			},
-        }
-    })
-	  lib.showContext('rw:jsbahan')
+		}
+	  })
+	  lib.registerContext({
+		id = 'joblist',
+		title = 'Menu Pekerjaan',
+		menu = 'rw:jsbahan1',
+		onBack = function()
+			lib.showContext('rw:jsbahan11')
+		end,
+		options = {
+			{
+				title = 'Aktif Kerja',
+				description = 'Aktif Untuk Menjadi Masyrakat',
+                		icon = 'hand',
+				event = 'rw:jobblips',
+			},
+		}
+	  })
+	  lib.showContext('rw:jsbahan11')
 end)
 
 -- [Ped Target in Ox_target!] --
